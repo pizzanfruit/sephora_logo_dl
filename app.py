8ef6b4062a99af1d9909dd632fa72ae755af2a2f
LongTH9 <longth.hust@gmail.com>
2017-12-08 15:45:13 +0900

    add samples

--- app.py
+++ app.py
@@ -17,12 +17,13 @@ def main():
     wb.sheet_names()
     sh = wb.sheet_by_index(0)
     brands = sh.col_values(0)
-    with open("output.txt", "a") as output:
+    with open("output.txt", "w") as output:
         for brand in brands:
             saveLogo(brand, directory, output)
 
 
 def saveLogo(brand, directory, output):
+    output.write("\n")
     domain_endpoint = "https://company.clearbit.com/v1/domains/find?name=" + brand
     headers = {
         'Authorization': 'Bearer sk_53839e6d7fbe624d0ea5d376ef3ea02e',
@@ -34,11 +35,13 @@ def saveLogo(brand, directory, output):
     print(o1)
     output.write(o1)
     if 'error' in r:
+        print("** No domain")
+        output.write("** No domain")
         return
     domain_name = r['domain']
     image_endpoint = 'https://logo.clearbit.com/' + domain_name
-    print(">> " + image_endpoint)
-    output.write(">> " + image_endpoint)
+    print(" >> " + image_endpoint)
+    output.write(" >> " + image_endpoint)
     try:
         image_file = directory + "/" + brand + ".png"
         urllib.request.urlretrieve(
@@ -47,8 +50,8 @@ def saveLogo(brand, directory, output):
         resized_img = img1.resize((500, 500), Image.ANTIALIAS)
         resized_img.save(image_file)
     except urllib.error.HTTPError:
-        print("** No logo")
-        output.write("** No logo")
+        print(" >> No logo")
+        output.write(" >> No logo")
         return
 
 
