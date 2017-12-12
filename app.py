9e5db2a96737b73ef19dc1777aae69249cf931f4
LongTH9 <longth.hust@gmail.com>
2017-12-13 01:26:41 +0900

    add google image scraping

--- app.py
+++ app.py
@@ -1,32 +1,102 @@
-import requests
-import urllib.request
+import json
 import os
-import xlrd
+import urllib.parse
+import urllib.request
+import psycopg2
+
 import PIL
+import requests
+import xlrd
+from azure.storage.blob import BlockBlobService, ContentSettings, PublicAccess
+from bs4 import BeautifulSoup
 from PIL import Image
 
 COUNT = 0
 
 
 def main():
-    directory = "logos"
+    directory = "logos2"
     if not os.path.exists(directory):
         os.makedirs(directory)
 
-    wb = xlrd.open_workbook("brands.xlsx")
-    wb.sheet_names()
-    sh = wb.sheet_by_index(0)
-    brands = sh.col_values(0)
-    with open("output.txt", "w") as output:
-        for brand in brands:
-            saveLogo(brand, directory, output)
+    # Download logos
+    # wb = xlrd.open_workbook("brands.xlsx")
+    # wb.sheet_names()
+    # sh = wb.sheet_by_index(0)
+    # brands = sh.col_values(0)
+    # with open("output.txt", "w") as output:
+    #     for brand in brands:
+    #         # save_logo(brand, directory, output)
+    #         scrape_google_image(brand, directory, output)
+    pg_dbname = os.environ["pg_dbname"]
+    pg_user = os.environ["pg_user"]
+    pg_host = os.environ["pg_host"]
+    pg_password = os.environ["pg_password"]
+    pg_connect_str = f"dbname='{pg_dbname}' user='{pg_user}' host='{pg_host}' " + \
+        f"password='{pg_password}'"
+
+    create_table_sql = """
+        CREATE TABLE IF NOT EXISTS MST_BRAND_LOGO(
+	        BRAND_NAME		  						VARCHAR(255) PRIMARY KEY,
+            LOGO_IMAGE_URL	                        VARCHAR(255) NOT NULL,    
+            CREATE_TIMESTAMP                        TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            CREATE_USER                             VARCHAR(255) NOT NULL,
+            UPDATE_TIMESTAMP                        TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            UPDATE_USER                             VARCHAR(255) NOT NULL
+        );
+    """
+    insert_sql = """
+        INSERT INTO MST_BRAND_LOGO(
+	        BRAND_NAME, LOGO_IMAGE_URL, CREATE_TIMESTAMP, CREATE_USER, UPDATE_TIMESTAMP, UPDATE_USER
+	        ) VALUES (%(brand_name)s, %(logo_image_url)s, current_timestamp, 'SYSTEM', current_timestamp, 'SYSTEM')
+	        ON CONFLICT (BRAND_NAME)
+	        DO UPDATE SET
+		    LOGO_IMAGE_URL = %(logo_image_url)s;
+    """
+    try:
+        conn = psycopg2.connect(pg_connect_str)
+        cursor = conn.cursor()
+    except Exception as e:
+        print("Invalid dbname, user or password?")
+        print(e)
+        return
+    cursor.execute(create_table_sql)
+    # Upload to Azure Blob
+    print(os.environ['azure_storage_account'])
+    print(os.environ['azure_storage_key'])
+    container_name = 'sephora-test'
+    block_blob_service = BlockBlobService(
+        account_name=os.environ['azure_storage_account'], account_key=os.environ['azure_storage_key'])
+    if not block_blob_service.exists("sephora-test"):
+        block_blob_service.create_container(
+            container_name, public_access=PublicAccess.Container)
+    for file in os.listdir(directory):
+        filename = os.fsdecode(file)
+        fullname = os.path.join(directory, filename)
+        brand, extension = os.path.splitext(filename)
+        content_type = 'image/' + extension
+        block_blob_service.create_blob_from_path(
+            container_name,
+            filename,
+            fullname,
+            content_settings=ContentSettings(content_type=content_type)
+        )
+        azure_logo_url = 'https://fsoftdtlp2diag974.blob.core.windows.net/' + \
+            container_name + '/' + urllib.parse.quote(filename)
+        cursor.execute(insert_sql, {'brand_name': brand,
+                                    'logo_image_url': azure_logo_url})
+        print(brand, azure_logo_url)
+    conn.commit()
+    cursor.close()
+    conn.close()
 
 
-def saveLogo(brand, directory, output):
+def save_logo(brand, directory, output):
     output.write("\n")
     domain_endpoint = "https://company.clearbit.com/v1/domains/find?name=" + brand
+    clearbit_api_key = os.environ["clearbit_api_key"]
     headers = {
-        'Authorization': 'Bearer sk_53839e6d7fbe624d0ea5d376ef3ea02e',
+        'Authorization': f'Bearer {clearbit_api_key}',
     }
     r = requests.get(domain_endpoint, headers=headers).json()
     global COUNT
@@ -35,11 +105,12 @@ def saveLogo(brand, directory, output):
     print(o1)
     output.write(o1)
     if 'error' in r:
-        print("** No domain")
-        output.write("** No domain")
+        print(" >> No domain")
+        output.write(" >> No domain")
+        scrape_google_image(brand, directory, output)
         return
     domain_name = r['domain']
-    image_endpoint = 'https://logo.clearbit.com/' + domain_name
+    image_endpoint = 'https://logo.clearbit.com/' + domain_name + "?size=800"
     print(" >> " + image_endpoint)
     output.write(" >> " + image_endpoint)
     try:
@@ -47,12 +118,68 @@ def saveLogo(brand, directory, output):
         urllib.request.urlretrieve(
             image_endpoint, image_file)
         img1 = Image.open(image_file)
-        resized_img = img1.resize((500, 500), Image.ANTIALIAS)
+        resized_img = img1.resize((800, 800), Image.ANTIALIAS)
         resized_img.save(image_file)
     except urllib.error.HTTPError:
         print(" >> No logo")
         output.write(" >> No logo")
+        scrape_google_image(brand, directory, output)
         return
+
+
+def scrape_google_image(brand, directory, output):
+    headers = {
+        'User-Agent': "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"}
+    encoded_brand = urllib.parse.quote(brand)
+    url = 'https://www.google.co.jp/search?q=' + \
+        encoded_brand + '+logo&tbas=0&tbm=isch&tbs=isz:lt,islt'
+    print(url)
+    page = requests.get(url, headers=headers)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    results = soup.find_all("div", {"class": "rg_meta"})
+    chosen = None
+    last_diff = 1
+    accept_type = ['jpg', 'jpeg', 'png']
+    for result in results[0:10]:
+        content = json.loads(result.text)
+        if not content['ity'].lower() in accept_type:
+            continue
+        width = content['ow']
+        height = content['oh']
+        ratio = height / width if width > height else width / height
+        diff = 1 - ratio
+        # if content['ity'] == 'png':
+        #     diff -= 0.2
+        if diff < last_diff:
+            chosen = content
+            last_diff = diff
+    extension = chosen['ity'] if chosen['ity'] else "png"
+    image_file = directory + "/" + brand + "." + extension
+    png_image_file = directory + "/" + brand + ".png"
+    response = requests.get(chosen['ou'], headers=headers)
+    print(chosen['ou'])
+    if response.status_code == 200:
+        with open(image_file, 'wb') as f:
+            f.write(response.content)
+    else:
+        print(" >> Google image fails")
+        output.write(" >> Google image fails")
+        return
+    # urllib.request.urlretrieve(
+    #     chosen, image_file)
+    print(image_file)
+    img1 = Image.open(image_file)
+    resized_img = img1.resize((800, 800))
+    try:
+        resized_img.save(image_file)
+    except OSError:
+        print(" >> Wrong format. Try to convert to png")
+        os.remove(image_file)
+        try:
+            resized_img.save(png_image_file)
+        except OSError:
+            print(" >> Fail to convert to png ")
+            return
 
 
 if __name__ == '__main__':
