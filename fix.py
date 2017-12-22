7ee209e91b52f2c49fdf2e480f7bf49a173d833e
LongTH9 <longth.hust@gmail.com>
2017-12-22 19:56:24 +0900

    use different resizing strategy

--- fix.py
+++ fix.py
@@ -5,31 +5,44 @@ from PIL import Image, ImageOps
 def main():
     need_fix_dir = "need_fix"
     fixed_dir = "fixed"
+    if not os.path.exists(fixed_dir):
+        os.makedirs(fixed_dir)
     for file in os.listdir(need_fix_dir):
         fullname = os.fsdecode(file)
+        if fullname[0] == ".":
+            continue
         resize(fullname, need_fix_dir, fixed_dir)
 
+
 def resize(fullname, input_dir, output_dir):
+    size = (800, 800)
     print(f"Resizing {fullname}. ", end='')
     filename, file_extension = os.path.splitext(fullname)
     orig_path = os.path.join(input_dir, fullname)
     fixed_path = os.path.join(output_dir, fullname)
     fixed_png_path = os.path.join(output_dir, filename + ".png")
-    img1 = Image.open(orig_path)
-    resized_img = ImageOps.fit(img1, (800, 800), Image.ANTIALIAS)
+    img = Image.open(orig_path)
+    height, width = img.size
+    max_dim = height if height > width else width
+    ratio = size[0] / max_dim
+    new_height = int(ratio * height)
+    new_width = int(ratio * width)
+    resized_img = img.resize((new_height, new_width), Image.ANTIALIAS)
+    background = Image.new('RGBA', size, (255, 255, 255, 0))
+    background.paste(
+        resized_img, (int((size[0] - new_height) / 2), int((size[0] - new_width) / 2)))
+    # resized_img = ImageOps.fit(img1, (800, 800), Image.ANTIALIAS)
+    # resized_img.thumbnail((800, 800), Image.ANTIALIAS)
     try:
-        resized_img.save(fixed_path)
-    except OSError as error:
+        background.save(fixed_path)
+    except OSError:
         print(" >> Wrong format. Try to convert to png")
-        print(error)
         if os.path.isfile(fixed_path):
-            print("why am i here")
             os.remove(fixed_path)
         try:
-            resized_img.save(fixed_png_path)
-        except OSError as error2:
+            background.save(fixed_png_path)
+        except OSError:
             print(" >> Fail to convert to png ")
-            print(error2)
             return
     print("Done!")
 
